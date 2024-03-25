fa40812bf8ee2bbd94868e05dbd891097c74224f
Johan Swan <jswannieman@gmail.com>
2024-03-25 13:44:18 +1300

    Commit index JS Component

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let cors = require("cors");
+let multer = require("multer");
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors());
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  let responseObj = {};
+  responseObj["name"] = req.file.originalname;
+  responseObj["type"] = req.file.mimetype;
+  responseObj["size"] = req.file.size;
+  res.json(responseObj);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
